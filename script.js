8e27d5e54e664b6a5befdeb1610fe936fabf7dd7
Pavel Karenda <korendos@gmail.com>
2020-03-15 19:33:30 +0300

    init: start task Singolo DOM

---
+++ script.js
@@ -0,0 +1,13 @@
+// BUTTON.addEventListener('click', (event) => {
+//   const subject = document.getElementById('subject').nodeValue.toString();
+//   document.getElementById('result').innerText = subject;
+// });
+
+
+const MENU = document.getElementById('menu');
+const TAGS = document.getElementById('tag-container');
+const MEDIA = document.getElementById('media')
+
+const BUTTON = document.getElementById('btn');
+const CLOSE_BUTTON = document.getElementById('close-btn');
+
