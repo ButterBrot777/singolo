b9810d4eafd0996de23726ab927e060c1349ad9e
Pavel Karenda <korendos@gmail.com>
2020-03-15 19:35:31 +0300

    feat: add changing the background of the menu and tags

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ const MEDIA = document.getElementById('media')
 const BUTTON = document.getElementById('btn');
 const CLOSE_BUTTON = document.getElementById('close-btn');
 
+MENU.addEventListener('click', (event) => {
+  console.log(MENU);
+  event.preventDefault();
+  if(event.target.classList.contains('header-link')) {
+    MENU.querySelectorAll('li>a').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+  }
+});
+
+// MENU.addEventListener('click', (event) => {
+//   console.log(MENU);
+//   console.log('ashki', MENU.querySelectorAll('li>a'));
+//   console.log('if pruve', event.target.classList.contains('header-link'));
+//   event.preventDefault();
+//   if(event.target.classList.contains('header-link')) {
+//     MENU.querySelectorAll('li>a').forEach(el => {
+//       console.log('in selector: ', el);
+//       el.target.style.color = '#white'});
+//     event.target.style.color = '#red';
+//   }
+// });
+
+console.log('TAG: ', TAGS)
+TAGS.addEventListener('click', (event) => {
+  event.preventDefault();
+  if(event.target.classList.contains('tag')) {
+    TAGS.querySelectorAll('a').forEach(el => el.parentNode.classList.remove('nav_container__featured'));
+    event.target.parentNode.classList.add('nav_container__featured');
+  }
+})
+
